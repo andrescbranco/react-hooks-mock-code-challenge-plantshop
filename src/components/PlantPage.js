6eac0f8bfcb8bf41a91fee356831dc29c861f98c
Andre Schnyder Castello Branco <andrecb18@yahoo.com.br>
2023-11-29 11:31:37 -0800

    Solution

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/plants')
+    .then((res)=>res.json())
+    .then((data)=>{
+      setPlants(data)
+      console.log(data)
+    })
+  },[])
+
+
+  function onAddPlant(newPlant) {
+    const newArray = [...plants,newPlant]
+    setPlants(newArray)
+  }
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  function onSearch(e){
+    setSearchTerm(e.target.value)
+  }
+  
+  const searchedPlants = plants.filter((plant)=>plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant}/>
+      <Search onSearch={onSearch} searchTerm={searchTerm} />
+      <PlantList plants={searchedPlants} />
     </main>
   );
 }
